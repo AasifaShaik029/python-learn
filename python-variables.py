a2fc4698bed212e250e677555277efe1f17c6ef3
EC2 Default User <ec2-user@ip-172-31-1-246.ap-south-1.compute.internal>
2022-05-25 07:48:45 +0000

    added python variable

---
+++ python-variables.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+#declaring int variable 
+
+intvar=4
+print(intvar)
+
+#2ways to declare float ways
+#1.initializing  decimal value for flaot variable
+
+floatvar=9.0
+print(floatvar)
+
+#2.cast with float for flaotvar
+
+floatvar=float(9)
+print(floatvar)
+
+#declaring string variable
+
+stringvar="Im learning python"
+print(stringvar)
+
+#Using single quotes(consider the  first and second quote as a string and ignore later part)
+
+#gives  syntax error
+
+#relation='I am my dad's daughter'
+
+#Using souble quotes(it includes single quotes in the string)
+
+relation="I am my dad's daughter"
+
+#To include double quotes in the string use single quotes
+
+paragraph='I am aasifa. working as "PA" in cognizant'
+paragraph="I am aasifa. working as \"PA\" in cognizant"
